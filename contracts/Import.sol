45f017d85d9919dc821ef7f86c3dd713afb5fc86
unenunciate <pmkelley@protonmail.com>
2022-09-25 11:20:56 -0400

    solidity

--- contracts/Import.sol
+++ contracts/Import.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.0;
 
 // TODO: get hardhat types from '@account-abstraction' and '@erc43337/common' package directly
 // only to import the file in hardhat compilation
-import "@erc4337/common/contracts/test/SampleRecipient.sol";
-import "@erc4337/common/contracts/test/SingletonFactory.sol";
+import "./SampleRecipient.sol";
+import "./SingletonFactory.sol";
 
 contract Import {
     SampleRecipient sampleRecipient;
